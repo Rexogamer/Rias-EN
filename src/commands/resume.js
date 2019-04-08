5253bd586500254ef02e2331d422f198c931cfa9
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-08 22:23:07 +0200

    Update resume.js

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -18,8 +18,8 @@ class Resume extends Command {
     // eslint-disable-next-line no-unused-vars
     async run(client, message, _args) {
         const player = client.player.get(message.guild.id);
-        if (!player) { return message.channel.send('❌  Le bot ne joue actuellement pas.'); }
-        if (player.playing) { return message.channel.send('❌  Le bot joue déjà.'); }
+        if (!player) { return message.channel.send('❌ Le bot ne joue actuellement pas.'); }
+        if (player.playing) { return message.channel.send('❌ Le bot joue déjà.'); }
             try {
                 await player.pause(false);
                 return message.channel.send('⏸ La musique est de nouveau en mode play.');
