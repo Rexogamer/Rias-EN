d520858c30c67423fcca3b9876312594d154df7c
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-08 20:21:00 +0200

    Update queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -16,7 +16,7 @@ class Queue extends Command {
     }
 
     // eslint-disable-next-line no-unused-vars
-    async run(client, message, _args) {
+    run(client, message, _args) {
         let queue = LavalinkFunctions.getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
         if (!client.player.get(message.guild.id)) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
         if (queue.length === 0) { return message.channel.send('❌ La queue est vide.'); }
