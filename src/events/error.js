63daf4165312cbabfd7ddd99a1863af92d64f16c
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-08 20:12:49 +0200

    Update error.js

--- src/events/error.js
+++ src/events/error.js
@@ -5,7 +5,7 @@ const Event = require('../structures/Event');
 module.exports = class Error extends Event {
     constructor() { super({ name: 'error' }); }
 
-    async run(client, error) {
+    run(client, error) {
         if (!error) { return; }
             client.logger.error(error);
     }
