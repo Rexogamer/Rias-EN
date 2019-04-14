3c08a66e44fbea7329205ebeb79d2791f7f55752
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-14 18:38:56 +0200

    Update play.js

--- src/commands/play.js
+++ src/commands/play.js
@@ -16,18 +16,12 @@ class Play extends Command {
     }
 
     run(client, message, args) {
-        let queue = LavalinkFunctions.getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
         if (!message.member || !message.member.voiceChannel) { return message.channel.send('❌ Vous devez être connecté dans un salon vocal pour faire cette commande.'); }
+            const player = client.player.get(message.guild.id);
             const track = args.join(' ');
-            if (!args.join(' ')) { return message.channel.send('❌ Vous devez spécifier un nom de musique.'); }
-            if (queue.length > 0) {
-                queue.push({ track, author: message.author.tag });
-                message.channel.send('☑ **' + track + '** a été ajouté avec succès à la queue !');
-            } else {
-                queue.push({ track, author: message.author.tag });
-                LavalinkFunctions.play(client, message, track);
-            }
-
+            if (!player) { return message.channel.send('❌ Le bot doit être connecté dans un salon-vocal avant.'); }
+            if (!track) { return message.channel.send('❌ Vous devez spécifier un nom de musique.'); }
+                LavalinkFunctions.addToQueue(client, message, track);
     }
 }
 
