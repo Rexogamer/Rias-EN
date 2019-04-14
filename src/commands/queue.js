4e792b75c9bd759df32ecae11d02fb5338fce66a
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-14 18:39:18 +0200

    Update queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -20,8 +20,8 @@ class Queue extends Command {
         let queue = LavalinkFunctions.getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
         if (!client.player.get(message.guild.id)) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
         if (queue.length === 0) { return message.channel.send('❌ La queue est vide.'); }
-            let text = queue.map((song, i) => (i > 0 && i < 15 ? '**' + i + '** - **' + song.track + '**' + ' - Added by **' + song.author + '**' : '')).join('\n');
-            message.channel.send('Voici la liste des musiques en attente du serveur:\n\n' + (queue.length === 1 ? '**La queue est vide !**' : text) + '\n\nLecture actuelle: **' + queue[0].track + '** - Ajoutée par **' + queue[0].author + '**')
+            let text = queue.map((song, i) => (i > 0 && i < 15 ? '**' + i + '** - **' + song.info.title + '**' + ' - Added by **' + song.author + '**' : '')).join('\n');
+            message.channel.send('Voici la liste des musiques en attente du serveur:\n\n' + (queue.length === 1 ? '**La queue est vide !**' : text) + '\n\nLecture actuelle: **' + queue[0].info.title + '** - Ajoutée par **' + queue[0].author + '**')
                 .catch((err) => {
                     if (err) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + err.message + '```'); }
                 });
