dafd62d749f50301b96aa104c2e06ae8d31f19ac
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-15 12:47:32 +0200

    Update clear.js

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Command = require('../structures/Command');
-const LavalinkFunctions = require('../utils/helpers/lavalink');
+const { getCurrentQueue } = require('../utils/helpers/lavalink');
 
 class Clear extends Command {
     constructor() {
@@ -17,7 +17,7 @@ class Clear extends Command {
 
     // eslint-disable-next-line no-unused-vars
     run(client, message, _args) {
-        let queue = LavalinkFunctions.getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
+        let queue = getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
         if (!client.player.get(message.guild.id)) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
         if (queue.length === 0) { return message.channel.send('⚠ La queue est vide.'); }
         else if (queue.length !== 1) { queue.splice(1, queue.length); }
