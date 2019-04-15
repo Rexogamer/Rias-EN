2abcc82a385c85c01d081811750796712338bdfb
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-15 12:47:04 +0200

    new command: replaynow

---
+++ src/commands/replaynow.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Command = require('../structures/Command');
+const { getCurrentQueue } = require('../utils/helpers/lavalink');
+
+class Replaynow extends Command {
+    constructor() {
+        super();
+        this.help = {
+            name: 'replaynow',
+            description: 'Remettre la lecture actuelle à 0',
+            category: 'Music',
+            usage: 'replaynow',
+            aliases: []
+        };
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    run(client, message, _args) {
+        let queue = getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
+        const player = client.player.get(message.guild.id);
+        if (!player) { return message.channel.send('❌ Le bot ne joue actuellement pas.'); }
+        if (queue.length === 0) { return message.channel.send('❌ La queue est vide.'); }
+            message.channel.send('⏮ Remise à 0...')
+                .then((m) => {
+                    m.delete();
+                    try {
+                        queue[0].loop = true;
+                        player.stop();
+                        setTimeout(() => {
+                            queue[0].loop = false;
+                        }, 1000)
+                    } catch (exception) {
+                        if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
+                    }
+                });
+    }
+}
+
+module.exports = Replaynow;
