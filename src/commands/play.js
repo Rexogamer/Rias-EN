1b319f0f97fa33faf4eca5fd5c76a0f47929b859
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-15 12:52:02 +0200

    Update play.js

--- src/commands/play.js
+++ src/commands/play.js
@@ -19,8 +19,14 @@ class Play extends Command {
         if (!message.member || !message.member.voiceChannel) { return message.channel.send('❌ Vous devez être connecté dans un salon vocal pour faire cette commande.'); }
             const player = client.player.get(message.guild.id);
             const track = args.join(' ');
-            if (!player) { return message.channel.send('❌ Le bot doit être connecté dans un salon-vocal avant.'); }
             if (!track) { return message.channel.send('❌ Vous devez spécifier un nom de musique.'); }
+            if (!player) {
+                client.player.join({
+                    guild: message.guild.id,
+                    channel: message.member.voiceChannel.id,
+                    host: client.player.nodes.first().host
+                }, { selfdeaf: true });
+            }
                 addToQueue(client, message, track);
     }
 }
