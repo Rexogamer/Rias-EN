138923f6195bddfe45da1cea9d0db4ecf71146c4
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-15 19:28:45 +0200

    Update volume.js

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -23,7 +23,10 @@ class Volume extends Command {
         else if (volume <= 0 || volume > 100) { return message.channel.send('❌ Vous devez spécifier un nombre compris entre **1** et **100** pour ajuster le volume.'); }
             try {
                 let vol = await player.volume(volume);
-                return message.channel.send('🔊 Le volume du stream est désormais à **' + vol.state.volume + '**.');
+                let progressBar = ['▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬', '▬'];
+                let calcul = Math.round(progressBar.length * (vol.state.volume / 100));
+                progressBar[calcul] = '🔘';
+                return message.channel.send('🔊 Le volume du stream est désormais à **' + vol.state.volume + '**%.\n[`' + vol.state.volume  + '%`] ' + progressBar.join('') + ' [`100%`]');
             } catch (exception) {
                 if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
             }    
