2d0a7c37211cf47b7482d084fb1a487fa69c2f77
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-16 13:50:01 +0200

    Update join.js

--- src/commands/join.js
+++ src/commands/join.js
@@ -15,19 +15,29 @@ class Join extends Command {
         };
     }
 
-    // eslint-disable-next-line no-unused-vars
-    run(client, message, _args) {
+    run(client, message, args) {
+        const query = args.join(' ');
+        const player = client.player.get(message.guild.id);
+        let channel;
         if (!message.member || !message.member.voiceChannel) { return message.channel.send('❌ Vous devez être connecté dans un salon vocal pour faire cette commande.'); }
-        try {
-            client.player.join({
-                guild: message.guild.id,
-                channel: message.member.voiceChannel.id,
-                host: client.player.nodes.first().host
-            }, { selfdeaf: true });
-            return message.channel.send('Le bot a bien rejoint le salon **' + message.member.voiceChannel.toString() + '**. 👌');
-        } catch (exception) {
-            if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
+        if (!message.member.voiceChannel && !query) { return message.channel.send('❌ Vous devez spécifier un salon !'); }
+        else if (query) {
+            channel = message.guild.channels.filter((c) => c.type === 'voice').find((c) => c.name.toLowerCase() === query.toLowerCase() || c.id === query);
+            if (!channel) { return message.channel.send('❌ Aucun salon trouvé !'); }
         }
+        else if (message.member.voiceChannel) { channel = message.member.voiceChannel; }
+        else { return message.channel.send('❌ Aucun salon trouvé !'); }
+        if (player) { return message.channel.send('❌ Le bot est déjà connecté dans un salon vocal.'); }
+            try {
+                client.player.join({
+                    guild: message.guild.id,
+                    channel: channel.id,
+                    host: client.player.nodes.first().host
+                }, { selfdeaf: true });
+                return message.channel.send('Le bot a bien rejoint le salon **' + channel.toString() + '**. 👌');
+            } catch (exception) {
+                if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
+            }
     }
 }
 
