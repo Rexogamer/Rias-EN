d704a2aba20b3c131389e2388ddd630335c96bbc
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-16 13:26:31 +0200

    new command: skipto

---
+++ src/commands/skipto.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Command = require('../structures/Command');
+const { getCurrentQueue } = require('../utils/helpers/lavalink');
+
+class Skipto extends Command {
+    constructor() {
+        super();
+        this.help = {
+            name: 'skipto',
+            description: 'Passer à une musique choisie dans la file d\'attente',
+            category: 'Music',
+            usage: 'skipto',
+            aliases: []
+        };
+    }
+
+    run(client, message, args) {
+        const choice = args.join(' ');
+        const player = client.player.get(message.guild.id);
+        let queue = getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
+        if (!choice || isNaN(choice)) { return message.channel.send('❌ Vous devez spécifier le nombre de la musique du classement de la file d\'attente.'); }
+        else if (choice <= 0 || choice > queue.length) { return message.channel.send('❌ Aucune musique ne possède cet identifiant dans la file d\'attente.'); }
+        if (!player) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
+            message.channel.send('⏩ Passage en cours...')
+                .then((m) => {
+                    m.delete();
+                    try {
+                        queue.splice(0, (choice-1));
+                        return player.stop();
+                    } catch (exception) {
+                        if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
+                    }
+                });
+    }
+}
+
+module.exports = Skipto;
