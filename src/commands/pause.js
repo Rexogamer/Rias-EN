9b382ab53ed4b819a9025c882416b9f3e9b16f52
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-16 12:11:25 +0200

    Update pause.js

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -16,12 +16,12 @@ class Pause extends Command {
     }
 
     // eslint-disable-next-line no-unused-vars
-    async run(client, message, _args) {
+    run(client, message, _args) {
         const player = client.player.get(message.guild.id);
         if (!player) { return message.channel.send('❌ Le bot ne joue actuellement pas.'); }
         if (!player.playing) { return message.channel.send('❌ Le bot ne joue actuellement pas.'); }
             try {
-                await player.pause(true);
+                player.pause(true);
                 return message.channel.send('⏸ La musique est maintenant en pause.');
             } catch (exception) {
                 if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
