a6ba168ec7459800339deedfdcbf42070d254df1
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-16 12:10:14 +0200

    Update leave.js

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -16,12 +16,12 @@ class Leave extends Command {
     }
 
     // eslint-disable-next-line no-unused-vars
-    async run(client, message, _args) {
+    run(client, message, _args) {
         let queue = getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
         if (!client.player.get(message.guild.id)) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
         if (queue.length > 0) { queue.splice(0, queue.length); }
             try {
-                await client.player.leave(message.guild.id);
+                client.player.leave(message.guild.id);
                 return message.channel.send('Le bot a bien quitté le salon vocal. 👌');
             } catch (exception) {
                 if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
