d5070744d03a05bf57c84e6d2a0389e92c00dc04
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-17 14:48:58 +0200

    Update seek.js

--- src/commands/seek.js
+++ src/commands/seek.js
@@ -25,9 +25,9 @@ class Seek extends Command {
         if (!query || isNaN(query)) { return message.channel.send('❌ Vous devez spécifier un nombre compris entre **1** et **' + (time / 1000) + '** pour modifier la position.'); }
         else if (query <= 0 || query > (time / 1000)) { return message.channel.send('❌ Vous devez spécifier un nombre compris entre **1** et **' + (time / 1000) + '** pour ajuster la position.'); }
             try {
-                let seek = player.seek((query * 1000));
                 let duration = moment.duration({ ms: time });
                 let progression = moment.duration({ ms: ((query * 1000) * 1000) });
+                player.seek((query * 1000));
                 return message.channel.send('⏩ La position est désormais à [`' + moment(progression/1000).minutes() + ':' + moment(progression/1000).seconds() + '`]/[`' + duration.minutes() + ':' + duration.seconds() + '`]');
             } catch (exception) {
                 if (exception) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + exception.message + '```'); }
