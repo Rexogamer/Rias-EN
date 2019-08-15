574b9d9592432ba632e8f84d5e4e287d334744ef
Ed Luff <beartechtalks@gmail.com>
2019-08-15 11:22:49 +0100

    Translated

--- src/commands/help.js
+++ src/commands/help.js
@@ -8,17 +8,17 @@ class Help extends Command {
         super();
         this.help = {
             name: 'help',
-            description: 'Voir la liste des commandes du bot',
+            description: "See a list of the bot's commands.",
             category: 'Bot',
             usage: 'help',
             aliases: ['h']
         };
     }
-
+q
     // eslint-disable-next-line no-unused-vars
     run(client, message, _args) {
         let commands = client.commands.filter((c) => c.help.category !== 'Owner').map((c) => '**[' + c.help.name + '](https://discordapp.com/)**\n*' + c.help.description + '.*').join('\n');
-        return message.channel.send('Voici la liste de mes commandes :smile:', {
+        return message.channel.send('Here are my commands! :smile:', {
             embed: {
                 author: {
                     name: client.user.username,
@@ -26,7 +26,7 @@ class Help extends Command {
                 },
                 description: commands,
                 footer: {
-                    text: 'par Sworder#0001'
+                    text: 'by Sworder#0001 and Rexowogamer#1183'
                 }
             }
         });
