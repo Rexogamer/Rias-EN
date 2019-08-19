07d1a9f28a254f7c21578cff4c0647e1522e0686
Ed Luff <beartechtalks@gmail.com>
2019-08-19 11:33:01 +0100

    translated

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -7,7 +7,7 @@ class Invite extends Command {
     super();
     this.help = {
       name: 'invite',
-      description: 'Envoie l\'invitation du bot',
+      description: 'Sends the bot\'s OAUTH link so you can invite it to your server.',
       category: 'Bot',
       usage: 'invite',
       aliases: []
@@ -16,7 +16,7 @@ class Invite extends Command {
 
   // eslint-disable-next-line no-unused-vars
   run(client, message, _args) {
-    message.channel.send(`🔗 Voici mon lien d\'invitation: <https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=36719872>`);
+    message.channel.send(`🔗 Here's the bot invite link: <https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=36719872>`);
   }
 }
 
