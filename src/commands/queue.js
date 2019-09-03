78c85e31a34d8044acaeb6c1bec47d350c866522
Ed Luff <beartechtalks@gmail.com>
2019-09-03 11:56:00 +0100

    translate queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -8,7 +8,7 @@ class Queue extends Command {
         super();
         this.help = {
             name: 'queue',
-            description: 'Voir la liste des musiques en attente',
+            description: 'Shows the music queue.',
             category: 'Music',
             usage: 'queue',
             aliases: []
@@ -18,13 +18,13 @@ class Queue extends Command {
     // eslint-disable-next-line no-unused-vars
     run(client, message, _args) {
         let queue = getCurrentQueue(client.config.LAVALINK.QUEUES, message.guild.id);
-        if (!client.player.get(message.guild.id)) { return message.channel.send('❌ Le bot n\'est actuellement pas connecté dans un salon vocal.'); }
-        if (queue.length === 0) { return message.channel.send('❌ La file d\'attente est vide.'); }
-            let text = queue.slice(0, 15).map((song, i) => (i > 0 && i < 15 ? '[**' + i + '**] - **' + song.info.title + '**' + ' - Ajouté par **' + song.author + '**' : null)).join('\n');
-            message.channel.send('Voici la liste des musiques en attente du serveur:\n\n' + (queue.length === 1 ? '**La queue est vide !**' : text + (queue.length > 15 ? '\n\nEt plus encore...' : '')) + '\n\nLecture actuelle: **' + queue[0].info.title + '** - Ajouté par **' + queue[0].author + '**')
+        if (!client.player.get(message.guild.id)) { return message.channel.send("❌ The bot isn't connected to a voice channel."); }
+        if (queue.length === 0) { return message.channel.send('❌ There are no songs in the queue.'); }
+            let text = queue.slice(0, 15).map((song, i) => (i > 0 && i < 15 ? '[**' + i + '**] - **' + song.info.title + '**' + ' - added by **' + song.author + '**' : null)).join('\n');
+            message.channel.send('**This is the queue:**\n\n' + (queue.length === 1 ? '**The queue is empty.**' : text + (queue.length > 15 ? '\n\nAnd more...' : '')) + '\n\nCurrently playing: **' + queue[0].info.title + '** - added by **' + queue[0].author + '**')
                 .catch((err) => {
-                    if (err) { return message.channel.send('❌ Une erreur est survenue, nous sommes désolé. Essayez plus tard.\n```JS\n' + err.message + '```'); }
-                });
+                    if (err) { return message.channel.send('❌ An error has occurred. Please try again later.\n```javascript\n' + err.message + '```'); }
+                }); AAAAAAAAAAAAA
     }
 }
 
